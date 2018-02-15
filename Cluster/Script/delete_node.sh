fadff0c04ba619281f304ac92ed692586e7c9a1f
Sakibul <sakib0202@gmail.com>
2018-02-15 09:44:09 +0600

    create master_add and check for correct node in delete_node

--- Cluster/Script/delete_node.sh
+++ Cluster/Script/delete_node.sh
@@ -6,6 +6,11 @@
 
 read -p "Enter the node name: " node
 
+if ! grep -q "$node " "/etc/hosts"; then
+    echo "Node not found. Please try again!!"
+    exit 1
+fi
+
 ip=$(cat /etc/hosts | grep "$node "  | awk '{print $1;}')
 nod=$(cat /etc/hosts | grep "$node " | awk '{print $2;}')
 
@@ -13,15 +18,16 @@ nod=$(cat /etc/hosts | grep "$node " | awk '{print $2;}')
 
 echo "Warning!! Following node will be deleted!!"
 
-echo -n
 echo "Node: $nod ; IP: $ip"
 read -p "Want to proceed? [y/n]: " confirm
 
 
 if [ "$confirm" == "y" ]; then
-		sed -i "/$nod /d" /etc/hosts
-		sed -i "/$ip /d" /etc/dnsmasq.conf
-		sed -i "/$nod /d" /home/nlocluster/node.conf
-        echo "$nod deleted successfully!!"
-	fi
+    sed -i "/$nod /d" /etc/hosts
+    sed -i "/$ip /d" /etc/dnsmasq.conf
+    sed -i "/$nod /d" /home/nlocluster/node.conf
+    echo "$nod deleted successfully!!"
+else
+    echo "Node not deleted. Please try again!!"
+fi
 
